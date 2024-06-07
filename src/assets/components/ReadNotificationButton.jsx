1d90542754dab9057e19bfa0bb9c8930f1050fa4
Francisco J. Casillas <chiccasillas@gmail.com>
2024-06-07 12:57:32 -0600

    Create ReadNotificationButton component

---
+++ src/assets/components/ReadNotificationButton.jsx
@@ -0,0 +1,7 @@
+function ReadNotificationButton({ decreaseNotificationsByOne }) {
+	return (
+		<button onClick={decreaseNotificationsByOne}>Read 1 Notification</button>
+	);
+}
+
+export default ReadNotificationButton;
