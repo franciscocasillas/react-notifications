484460a5033f6155586dca9ce8d45a1702f96b59
Francisco J. Casillas <chiccasillas@gmail.com>
2024-06-07 12:59:24 -0600

    Create ReceiveNotification component

---
+++ src/assets/components/ReceiveNotificationButton.jsx
@@ -0,0 +1,7 @@
+function ReceiveNotificationButton({ increaseNotificationsByOne }) {
+	return (
+		<button onClick={increaseNotificationsByOne}>Receive 1 Notification</button>
+	);
+}
+
+export default ReceiveNotificationButton;
